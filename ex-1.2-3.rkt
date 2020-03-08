5233075299538410688df4a8441509e21955f43e
erickbot <ericksilvestrelimadebrito@gmail.com>
2020-03-08 15:48:25 -0300

    Exercícios

--- ex-1.2-3.rkt
+++ ex-1.2-3.rkt
@@ -3,7 +3,7 @@
 ;; exercicio 1.2
 ;; Ver em https://web.mit.edu/alexmv/6.037/sicp.pdf 
 
-...
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
 
 ;; exercicio 1.3
 
@@ -14,7 +14,10 @@
   (+ (square x) (square y)))
 
 (define (sum-of-squares-max3 a b c)
-  ...)
+  (cond
+    ((and (> a b) (> b c)) (sum-of-squares a b))
+    ((and (> a b) (< b c)) (sum-of-squares a c))
+    (else (sum-of-squares b c))))
 
 (provide sum-of-squares-max3)
 
